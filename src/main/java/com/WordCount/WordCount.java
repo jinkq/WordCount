327f4c6240ed221e488aeaf0d38e5379f0bfd570
jinkq <1154609706@qq.com>
2020-10-28 22:14:28 +0800

    成功在伪分布模式运行

--- src/main/java/com/WordCount/WordCount.java
+++ src/main/java/com/WordCount/WordCount.java
@@ -10,10 +10,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.Random;
+import java.util.regex.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -23,6 +26,14 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.StringUtils;
+
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.map.InverseMapper;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.WritableComparable;
+
 
 public class WordCount {
 
@@ -35,7 +46,8 @@ public class WordCount {
     private Text word = new Text();
 
     private boolean caseSensitive;
-    private Set<String> patternsToSkip = new HashSet<String>();
+    private Set<String> patternsToSkip = new HashSet<String>();//停词
+    private Set<String> punctuations = new HashSet<String>();//标点符号
 
     private Configuration conf;
     private BufferedReader fis;
@@ -47,12 +59,25 @@ public class WordCount {
       caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
       if (conf.getBoolean("wordcount.skip.patterns", false)) {
         URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
-        for (URI patternsURI : patternsURIs) {
-          Path patternsPath = new Path(patternsURI.getPath());
-          String patternsFileName = patternsPath.getName().toString();
-          parseSkipFile(patternsFileName);
-        }
+        
+        URI patternsURI = patternsURIs[0];
+        Path patternsPath = new Path(patternsURI.getPath());
+        String patternsFileName = patternsPath.getName().toString();
+        parseSkipFile(patternsFileName);
+
+        URI punctuationsURI = patternsURIs[1];
+        Path punctuationsPath = new Path(punctuationsURI.getPath());
+        String punctuationsFileName = punctuationsPath.getName().toString();
+        parsePunctuations(punctuationsFileName);
+
+        // for (URI patternsURI : patternsURIs) {
+        //   Path patternsPath = new Path(patternsURI.getPath());
+        //   String patternsFileName = patternsPath.getName().toString();
+        //   parseSkipFile(patternsFileName);
+        // }
       }
+      // parseSkipFile("skip/stop-word-list.txt");
+      // parsePunctuations("skip/punctuations.txt");
     }
 
     private void parseSkipFile(String fileName) {
@@ -68,17 +93,55 @@ public class WordCount {
       }
     }
 
+    private void parsePunctuations(String fileName) {
+      try {
+        fis = new BufferedReader(new FileReader(fileName));
+        String punctuation = null;
+        while ((punctuation = fis.readLine()) != null) {
+          punctuations.add(punctuation);
+        }
+      } catch (IOException ioe) {
+        System.err.println("Caught exception while parsing the cached file '"
+            + StringUtils.stringifyException(ioe));
+      }
+    }
+
+    private Boolean isNumeric(String str) {
+      Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+      return pattern.matcher(str).matches();
+    }
+
     @Override
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      String line = (caseSensitive) ?
-          value.toString() : value.toString().toLowerCase();
-      for (String pattern : patternsToSkip) {
-        line = line.replaceAll(pattern, "");
+      String line = value.toString().toLowerCase(); //全部转小写，不用命令行判断
+      for (String pattern : punctuations) {
+        line = line.replaceAll(pattern, " ");
       }
       StringTokenizer itr = new StringTokenizer(line);
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
+      while (itr.hasMoreTokens()) {//单词
+        String str = itr.nextToken();
+        Boolean shouldRemove = false;
+
+        //判断数字、长度
+        if(isNumeric(str)||str.length()<3)
+        {
+          continue;
+        }
+
+        // 判断是不是停词，
+        for(String pattern : patternsToSkip) {
+          if(pattern.equals(str)) {
+            shouldRemove = true;
+            break;
+          }
+        }
+        
+        if (shouldRemove) {
+          continue;
+        }
+        // word.set(itr.nextToken());
+        word.set(str);
         context.write(word, one);
         Counter counter = context.getCounter(CountersEnum.class.getName(),
             CountersEnum.INPUT_WORDS.toString());
@@ -103,14 +166,44 @@ public class WordCount {
     }
   }
 
+  private static class IntWritableDecreasingComparator extends IntWritable.Comparator {  
+      public int compare(WritableComparable a, WritableComparable b) {  
+        return -super.compare(a, b);  
+      }  
+
+      public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {  
+          return -super.compare(b1, s1, l1, b2, s2, l2);  
+      }  
+  }
+
+  public static class SortReducer extends Reducer<IntWritable, Text, Text, NullWritable>{
+    private Text result = new Text();
+    int rank=1;
+
+    @Override
+    protected void reduce(IntWritable key, Iterable<Text> values, Context context) 
+     throws IOException, InterruptedException{
+      for(Text val: values){
+        if(rank > 100)
+        {
+          break;
+        }
+        result.set(val.toString());
+        String str=rank+": "+result+", "+key;
+        rank++;
+        context.write(new Text(str),NullWritable.get());
+      }
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
     String[] remainingArgs = optionParser.getRemainingArgs();
-    if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
-      System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
-      System.exit(2);
-    }
+    // if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
+    //   System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
+    //   System.exit(2);
+    // }
     Job job = Job.getInstance(conf, "word count");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
@@ -123,14 +216,49 @@ public class WordCount {
     for (int i=0; i < remainingArgs.length; ++i) {
       if ("-skip".equals(remainingArgs[i])) {
         job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+        job.addCacheFile(new Path(remainingArgs[++i]).toUri());
         job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
       } else {
         otherArgs.add(remainingArgs[i]);
       }
     }
     FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
-    FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
 
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    Path tempDir = new Path("tmp-" + Integer.toString(  
+            new Random().nextInt(Integer.MAX_VALUE))); //第一个job的输出写入临时目录
+    // FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+    FileOutputFormat.setOutputPath(job, tempDir);
+
+    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+    // System.exit(job.waitForCompletion(true) ? 0 : 1);
+   
+
+    if(job.waitForCompletion(true))  
+    {  
+        //新建一个job处理排序和输出格式
+        Job sortJob = new Job(conf, "sort");  
+        sortJob.setJarByClass(WordCount.class);  
+
+        FileInputFormat.addInputPath(sortJob, tempDir); 
+
+        sortJob.setInputFormatClass(SequenceFileInputFormat.class);  
+        
+        //map后交换key和value
+        sortJob.setMapperClass(InverseMapper.class);  
+        sortJob.setReducerClass(SortReducer.class);
+        
+        FileOutputFormat.setOutputPath(sortJob, new Path(otherArgs.get(1)));  
+
+        sortJob.setOutputKeyClass(IntWritable.class);  
+        sortJob.setOutputValueClass(Text.class); 
+
+        //排序改写成降序
+        sortJob.setSortComparatorClass(IntWritableDecreasingComparator.class);  
+
+        System.exit(sortJob.waitForCompletion(true) ? 0 : 1); 
+    }  
+
+    FileSystem.get(conf).deleteOnExit(tempDir);
   }
 }
